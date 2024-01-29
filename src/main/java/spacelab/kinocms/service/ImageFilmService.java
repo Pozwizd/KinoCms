c562e715fd1165156b256b967d483daf68990561
Belwerk <glyuchnoglaz21@gmail.com>
2024-01-29 19:31:20 +0200

    Added validation for editUser page

---
+++ src/main/java/spacelab/kinocms/service/ImageFilmService.java
@@ -0,0 +1,29 @@
+package spacelab.kinocms.service;
+
+import org.springframework.stereotype.Service;
+import spacelab.kinocms.model.Cinema;
+import spacelab.kinocms.model.Film;
+import spacelab.kinocms.model.ImagesEntity.ImageCinema;
+import spacelab.kinocms.model.ImagesEntity.ImageFilm;
+
+import java.util.List;
+
+@Service
+public interface ImageFilmService {
+
+    public void saveImageFilm(ImageFilm imageFilm);
+
+    public void deleteImageFilm(Long id);
+
+    public ImageFilm getImageFilmById(Long id);
+
+    public ImageFilm getLastImageFilmByFilm(Film film);
+
+    public List<ImageFilm> getAllImageFilm();
+
+    public List<ImageFilm> getAllImageFilmByFilm(Film film);
+
+    public void updateImageFilm(ImageFilm imageFilm);
+
+
+}
